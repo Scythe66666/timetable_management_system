0f9d30f87b8d1a87033ae310f972976d91d4627f
Scythe66666 <riteshdavkare67@gmail.com>
2024-03-08 16:57:06 +0530

    1]updated pom.xml to remove .h2 database dependency and put sqlite jdbc   dependency from maven repository instead of downloaded jar file 2]created a seprate header.html which will be put in code using thymleaf   replace 3]divided some functionality of time_slot.java with time_table.java

--- src/main/java/com/example/form_creation/timetable.java
+++ src/main/java/com/example/form_creation/timetable.java
@@ -1,47 +1,7 @@
 package com.example.form_creation;
 
 import java.sql.*;
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Objects;
 
-class time_slot {
-    public List<String> list_lectures = new ArrayList<>();
-    public List<String> list_classrooms = new ArrayList<>();
-    int i = 0;
-    public void append_lecture(String subject, String classroom) {
-        i++;
-        list_lectures.add(subject);
-        list_classrooms.add(classroom);
-    }
-    public int geti(){
-        return i;
-    }
-    public time_slot() {
-        
-    }
-
-    public List<String> getList_classrooms() {
-        return this.list_classrooms;
-    }
-
-    public List<String> getList_lectures() {
-        return this.list_lectures;
-    }
-
-    public void setList_lectures(List<String> list_lectures) {
-        this.list_lectures = list_lectures;
-    }
-
-    @Override
-    public String toString() {
-        return "{" +
-            " list_lectures='" + getList_lectures() + "'" +
-            ", list_classrooms='" + getList_classrooms() + "'" +
-            "}";
-    }
-
-}
 
 public class timetable {
     private String Connection_string = "jdbc:sqlite:" + System.getProperty("user.dir")
