c7080b340e3acf04e45d58b74189b68f00f66cd5
Scythe66666 <riteshdavkare67@gmail.com>
2024-03-08 23:13:56 +0530

    1]removed the spring_jdbc dependency and added sqlite_jdbc dependency 2]divide the timetable code to service, repository and controller layer 3]did some minor bug fix in header.html 4]started adding functionality to the timetable.html

--- src/main/java/com/example/form_creation/time_slot.java
+++ src/main/java/com/example/form_creation/time_slot.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.Objects;
 
-class time_slot {
+public class time_slot {
     public List<String> list_lectures = new ArrayList<>();
     public List<String> list_classrooms = new ArrayList<>();
     int i = 0;
@@ -41,24 +41,4 @@ class time_slot {
             "}";
     }
 
-}
-
-public class timetable {
-    private String Connection_string = "jdbc:sqlite:" + System.getProperty("user.dir")
-            + "/src/main/resources/static/database/";
-    private Connection con;
-    private String db_name;
-
-    public timetable(String Db_name) throws Exception {
-        db_name = Db_name;
-        Class.forName("org.sqlite.JDBC");
-        con = DriverManager.getConnection(Connection_string + Db_name);
-        Statement stm = con.createStatement();
-    }
-
-    ResultSet giveResultSet(String table_name) throws Exception {
-        Statement stm = con.createStatement();
-        ResultSet rs = stm.executeQuery("select * from " + table_name);
-        return rs;
-    }
 }
